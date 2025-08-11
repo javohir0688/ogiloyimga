4ec60aa7c2edb995711ffb7d2f505199a5bf5a0b
javohir0688 <abdusattorovjavohir06@gmail.com>
2025-08-12 00:04:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const closeBtn = document.getElementById('closeBtn');
 const message = document.getElementById('message');
 
 openBtn.addEventListener('click', () => {
-  message.classList.add('message-visible');
+  message.classList.remove('hidden');
+  message.classList.add('visible');
   openBtn.style.display = 'none';
   closeBtn.style.display = 'inline-block';
 });
 
 closeBtn.addEventListener('click', () => {
-  message.classList.remove('message-visible');
+  message.classList.remove('visible');
+  message.classList.add('hidden');
   closeBtn.style.display = 'none';
   openBtn.style.display = 'inline-block';
 });
